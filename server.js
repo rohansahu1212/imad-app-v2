17af6c6b0ac1a25771310dd223954163f13fa271
rohansahu1212 <rohansahu1212@gmail.com>
2017-02-14 17:33:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+app.get('/article-one',function(req,res)
+{
+    res.send('article one is requested');
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
