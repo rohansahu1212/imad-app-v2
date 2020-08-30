20592f62515a66ec48cec38827b90db1912c0d69
rohansahu1212 <rohansahu1212@gmail.com>
2020-08-30 20:45:36 +0530

    java file
    java code here

---
+++ mca.java
@@ -0,0 +1,13 @@
+class mca{
+public static void main(String[] args)
+{
+ int a,b,c;
+ a=10;
+ b=14;
+ c=a+b;
+System.out.println(c);
+}
+}
+ //javac filename.java   //for compile
+ //java filename         // for run \
+ // set path = "C:\Program Files (x86)\Java\jdk1.6.0_10\bin"  
